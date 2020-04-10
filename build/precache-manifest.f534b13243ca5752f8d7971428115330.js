30563e614543a29aeffe75761819ee7f7f41c9a9
Admin <cipher@MacBook-Pro-sszruk.local>
2020-04-10 22:56:12 +0300

    test commit with build

---
+++ build/precache-manifest.f534b13243ca5752f8d7971428115330.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1946983b408d6f2601beeea57637fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26429093a86d7d3f68a3",
+    "url": "/static/css/main.dbf97021.chunk.css"
+  },
+  {
+    "revision": "4c98b7deca71393285a6",
+    "url": "/static/js/2.a2f0555b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.a2f0555b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26429093a86d7d3f68a3",
+    "url": "/static/js/main.c2e9250b.chunk.js"
+  },
+  {
+    "revision": "452efe56c2036ff11208",
+    "url": "/static/js/runtime-main.83c66be5.js"
+  }
+]);
