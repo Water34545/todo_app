3975378f9cb053088ff309e5aaec2b680db449ae
Admin <cipher@MacBook-Pro-sszruk.local>
2020-04-10 23:54:11 +0300

    change package.json and rebuild

---
+++ build/precache-manifest.a035692b2e66ddf986a4ba0dca12ac0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9954ab97c5677cec0b46076018b8590c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "243ea367c0357015c47e",
+    "url": "./static/css/main.dbf97021.chunk.css"
+  },
+  {
+    "revision": "e12526478c1fc019b573",
+    "url": "./static/js/2.333cf6d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.333cf6d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "243ea367c0357015c47e",
+    "url": "./static/js/main.f7dbe513.chunk.js"
+  },
+  {
+    "revision": "119396d1953e59a62ea1",
+    "url": "./static/js/runtime-main.0d4c0a39.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f534b13243ca5752f8d7971428115330.js"
+  "./precache-manifest.a035692b2e66ddf986a4ba0dca12ac0e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
